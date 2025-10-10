bceee5f7ca7e24f1da27bf9a2cacb88aa10fa90c
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-10-10 15:49:00 +0200

    Add docker_utils.py docstrings.

--- phylorun/utils/docker_utils.py
+++ phylorun/utils/docker_utils.py
@@ -8,6 +8,7 @@ from loguru import logger
 
 
 def get_docker_client() -> docker.DockerClient:
+    """Instantiate and return a Docker client."""
     try:
         return docker.from_env()
     except DockerException:
@@ -20,6 +21,7 @@ def get_docker_client() -> docker.DockerClient:
 def create_image_if_needed(
     client: docker.DockerClient, image_name: str, docker_file: str
 ):
+    """Ensure a Docker image exists, building it from a Dockerfile string if necessary."""
     try:
         client.images.get(image_name)
         # image already created
@@ -45,6 +47,16 @@ def create_image_if_needed(
 def start_container(
     client: docker.DockerClient, image_name: str, **kwargs
 ) -> Container:
+    """Start a Docker container from a specified image.
+
+    Args:
+        client (docker.DockerClient): The Docker client.
+        image_name (str): Name of the Docker image.
+        **kwargs: Additional keyword arguments passed to container run.
+
+    Returns:
+        Container: The started Docker container.
+    """
     return client.containers.run(
         image_name, "sleep infinity", detach=True, platform="linux/x86_64", **kwargs
     )
@@ -56,6 +68,14 @@ def run_and_print_command(
     user: Optional[str] = None,
     working_dir: Optional[str] = None,
 ):
+    """Run a command inside a Docker container and print its output to stdout and stderr.
+
+    Args:
+        container (Container): The Docker container where the command will be executed.
+        command (str): The command to execute.
+        user (Optional[str]): User to run the command as (if specified).
+        working_dir (Optional[str]): The working directory inside the container (if specified).
+    """
     result = container.exec_run(
         command, stream=True, demux=True, user=user, workdir=working_dir
     )
